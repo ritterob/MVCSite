a429549e69ae0490b9c13f1a144ac59649e9dc28
Rob Ritter <ritterhaus@gmail.com>
2023-06-06 07:04:02 -0500

    Add project files.

---
+++ MVCSite/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MVCSite.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
