da4ad7bc4713f9fb14d832d061ed5ef4fcb809d6
Rob Ritter <ritterhaus@gmail.com>
2023-06-12 08:03:53 -0500

    Updates

--- MVCSite/Controllers/HomeController.cs
+++ MVCSite/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCSite.Models;
-using System.Diagnostics;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace MVCSite.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, IConfiguration config) {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index() {
@@ -30,9 +33,43 @@ namespace MVCSite.Controllers {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm) {
+
+            if (!ModelState.IsValid) { return View(cvm); }
+
+            string message = string.Format("You recieved a message from {0} ({1}) about {2}.<br/>" +
+                "Message:<br/>{3}", cvm.Name, cvm.Email, cvm.Subject, cvm.Message);
+
+            var mm = new MimeMessage();
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+            mm.Subject = cvm.Subject;
+            mm.Body = new TextPart("HTML") { Text = message };
+            mm.Priority = MessagePriority.Urgent;
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new SmtpClient()) {
+
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"));
+                client.Authenticate(_config.GetValue<string>("Credentials:Email:User"),
+                    _config.GetValue<string>("Credentials:Email:Password"));
+                try {
+                    client.Send(mm);
+                }
+                catch (Exception e) {
+
+                   ViewBag.Error = string.Format("There was an error sending your message. Please try " +
+                       "again later.<br/>Error message: {0}", e.StackTrace);
+                    return View(cvm);
+                }
+
+            }   // end using directive
+
+            return View("EmailConfirmation", cvm);
+
+        }   // end IActionResult Contact(ContactViewModel)
+
+    }   // end class HomeController
+
 }
---
+++ MVCSite/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCSite.Models {
+    public class ContactViewModel {
+
+        [Required(ErrorMessage = "* Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public string Message { get; set; }
+
+    }   // end class ContactViewModel
+}
